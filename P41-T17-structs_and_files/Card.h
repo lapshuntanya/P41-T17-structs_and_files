3af0e7e8259f317b03182504a0b67bff2748f973
lapshuntanya <lapshun.tatiana@gmail.com>
2025-01-22 17:01:06 +0200

    saveToBinFile

--- P41-T17-structs_and_files/Card.h
+++ P41-T17-structs_and_files/Card.h
@@ -16,12 +16,29 @@ struct Card
 		number = rand() * 1'000'000 + rand();
 		cash = 1'000;
 	}
-	
 	void showCard() {
 		cout << "+++++++++++++++++++++++++++++\n";
 		cout << "|\tClient: " << name << endl;
 		cout << "|\tCard:   " << number << endl;
 		cout << "|\tCash:   " << cash << " UAH" << endl;
 		cout << "+++++++++++++++++++++++++++++\n";
+	}
+
+	void saveToBinFile(FILE* file) {
+		int lengthName = strlen(name) + 1;
+		fwrite(&lengthName, sizeof(int), 1, file);//save name
+		fwrite(name, sizeof(char), lengthName, file);
+
+		fwrite(&number, sizeof(unsigned long), 1, file);//save number
+		fwrite(&cash, sizeof(double), 1, file);//save cash
+	}
+	
+	void loadFromBinFile(FILE* file) {
+		int lengthName = 0;
+		fread(&lengthName, sizeof(int), 1, file);//load name
+		fread(name, sizeof(char), lengthName, file);
+
+		fread(&number, sizeof(unsigned long), 1, file);//load number
+		fread(&cash, sizeof(double), 1, file);//load cash
 	}
 };
