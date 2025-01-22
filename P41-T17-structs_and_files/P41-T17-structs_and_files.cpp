a52eded63ae6e62dbabefa7737f71264ea9d3365
lapshuntanya <lapshun.tatiana@gmail.com>
2025-01-22 17:33:14 +0200

    main

--- P41-T17-structs_and_files/P41-T17-structs_and_files.cpp
+++ P41-T17-structs_and_files/P41-T17-structs_and_files.cpp
@@ -1,5 +1,5 @@
 #include "Card.h"
-#define LOAD_OR_CREATE 0 //1-load, 0-create
+#define LOAD_OR_CREATE 1 //1-load, 0-create
 
 int main()
 {
@@ -11,13 +11,20 @@ int main()
 #if LOAD_OR_CREATE == 0
     Yehor.createCard();
 #else
-    //load
+    fopen_s(&file_bank, "bank.bin", "rb");
+    if (file_bank == nullptr) cout << "Error: not loaded!\n";
+    else {
+        Yehor.loadFromBinFile(file_bank);
+        fclose(file_bank);
+        cout << "Loaded!\n";
+    }
 #endif // LOAD_OR_CREATE == 0
-  
+   //---------------------------------------------------------- 
     Yehor.showCard();
     Yehor.cash += 0.99;
 
-    //Save to file
+    //----------------------------------------------------------
+    // Save to file
     fopen_s(&file_bank, "bank.bin", "wb");
     if (file_bank == nullptr) cout << "Error: not saved!\n";
     else {
